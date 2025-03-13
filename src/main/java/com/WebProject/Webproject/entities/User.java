1a6085ff77a8e191dfafd6d65ab139e626c503aa
Ramon Rocha <ramon.rocha@compasso.com.br>
2025-03-13 08:49:31 -0300

    feat: create Users

---
+++ src/main/java/com/WebProject/Webproject/entities/User.java
@@ -0,0 +1,86 @@
+package com.WebProject.Webproject.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private long id;
+    private String nome;
+    private String email;
+    private String phone;
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(long id, String nome, String email, String phone, String password) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+
+
+
+}
+
---
+++ src/main/java/com/WebProject/Webproject/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.WebProject.Webproject.resources;
+
+
+import com.WebProject.Webproject.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "32714011", "12345");
+        return ResponseEntity.ok().body(u);
+
+    }
+
+    ;
+}
