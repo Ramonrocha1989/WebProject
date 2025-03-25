a2706734f10b22656b023abac4aeac0aa37006dc
Ramon Rocha <ramon.rocha@compasso.com.br>
2025-03-25 16:46:02 -0300

    Exception handling - update

--- src/main/java/com/WebProject/Webproject/services/UserService.java
+++ src/main/java/com/WebProject/Webproject/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import com.WebProject.Webproject.services.exceptions.DatabaseException;
 import com.WebProject.Webproject.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.WebProject.Webproject.entities.User;
@@ -44,9 +45,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
